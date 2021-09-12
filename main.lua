bbfc5df68a1965812833f8851d0db09c3ea65bad
phrbmr <73762126+phrbmr@users.noreply.github.com>
2021-09-12 01:05:45 +0000

    pong

--- main.lua
+++ main.lua
@@ -1,13 +1,23 @@
--- Defines a recursive factorial function 
-function fact (n)
-  if n == 0 then
-    return 1
-  else
-    return n * fact(n-1)
-  end
+--[[
+  Used to initialize the game. Runs when the game first starts up, only once.
+]]
+function love.load()
+love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+  fullscreen = false,
+  resizable = false,
+  vsync = true
+  })
 end
 
-print("Hello World!")
-print("Please dear Sir, enter a number: ")
-a = io.read("*number") -- read the number
-print(fact(a))
+--[[
+  Called after update by LÖVE2D, used to draw anything to the screen, called each frame
+  after update.
+]]
+function love.draw()
+  love.graphics.printf(
+    'Hello Pong!',          -- text render
+    0,                      -- starting X (0 to center it)
+    WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
+    WINDOW_WIDTH,           -- number of pixels to center within
+    'center')               -- alignment mode, can be 'center, 'left' or 'right'
+end
